da3fdf4fcd7409287d734a54311c884f9b43e84a
Sara-MF <sara.melo@aluno.uece.br>
2023-02-26 21:18:55 -0300

    feat: showing profile description on hover

--- src/pages/sign-up/profile.tsx
+++ src/pages/sign-up/profile.tsx
@@ -1,6 +1,7 @@
 import Layout from "components/Layout";
 import Link from "next/link";
 import styled from "styled-components";
+import { useState } from "react";
 
 const ChooseProfileTitle = styled.div`
     text-align: center;
@@ -10,7 +11,7 @@ const ChooseProfileTitle = styled.div`
     border-radius: 1rem;
 `
 
-const ChooseProfileContainer = styled.div`
+const ViewProfile = styled.div`
     width: 80vw;
     display: flex;
     flex-direction: row;
@@ -19,63 +20,137 @@ const ChooseProfileContainer = styled.div`
     justify-content: space-between;
 `
 
-const ProfileContainer = styled.div`
+const NameProfile = styled.div`
     width: 20vw;
     height: 20vw;
 `
 
-const ProfileDescriptionContainer = styled.div`
-    width: 60vw;
-    height: 10vh;
+const DescriptionProfile = styled.div`
+    width: 65vw;
+    height: 8vh;
     margin: auto;
     margin-top: 10vh;
     display: none;
+    text-align: center;
+    align-items: center;
+    padding-top: 2vh;
+`
+
+const ChooseProfile = styled.div`
+    display: flex;
+    flex-direction: column;
 `
 
 const Profile = () => {
+
+    // Client Description
+    function showClientDescription() {
+        if (typeof window !== 'undefined') {
+
+            const showClientDescription = document.querySelector(".description-client-profile");
+            showClientDescription.style.display = "block";
+
+        }
+    }
+
+    function hideClientDescription() {
+        if (typeof window !== 'undefined') {
+
+            const hideClientDescription = document.querySelector(".description-client-profile");
+            hideClientDescription.style.display = "none";
+        }
+    }
+
+    // Realtor Description
+    function showRealtorDescription() {
+        if (typeof window !== 'undefined') {
+
+            const showRealtorDescription = document.querySelector(".description-realtor-profile");
+            showRealtorDescription.style.display = "block";
+
+        }
+    }
+
+    function hideRealtorDescription() {
+        if (typeof window !== 'undefined') {
+
+            const hideRealtorDescription = document.querySelector(".description-realtor-profile");
+            hideRealtorDescription.style.display = "none";
+        }
+    }
+
+    // Agency Description
+    function showAgencyDescription() {
+        if (typeof window !== 'undefined') {
+
+            const showAgencyDescription = document.querySelector(".description-agency-profile");
+            showAgencyDescription.style.display = "block";
+
+        }
+    }
+
+    function hideAgencyDescription() {
+        if (typeof window !== 'undefined') {
+
+            const hideAgencyDescription = document.querySelector(".description-agency-profile");
+            hideAgencyDescription.style.display = "none";
+        }
+    }
+
     return (
+
         <Layout>
 
             <ChooseProfileTitle>
                 <h1>Selecione o tipo de perfil que deseja criar</h1>
             </ChooseProfileTitle>
 
-            <ChooseProfileContainer className="choose-profile">
+            <ChooseProfile className="choose-profile">
 
-                <Link className="link choose-profile-client" href="/sign-up">
+                <ViewProfile className="view-profile">
 
-                    <ProfileContainer className="card choose-profile-client">
+                    <Link className="link choose-profile-client" href="/sign-up" // const sign up default
+                        onMouseEnter={showClientDescription}
+                        onMouseLeave={hideClientDescription}>
 
-                        <button className="profileButton">Cliente</button>
+                        <NameProfile className="card choose-profile-client">
+                            <button className="profileButton">Cliente</button>
+                        </NameProfile>
 
-                    </ProfileContainer>
-                </Link>
+                    </Link>
 
-                <Link className="link choose-profile-realtor" href="/sign-up">
-                    <ProfileContainer className="card choose-profile-realtor">
-                        <button className="profileButton">Consultor</button>
-                    </ProfileContainer>
-                </Link>
+                    <Link className="link choose-profile-realtor" href="/sign-up" // const sign up default
+                        onMouseEnter={showRealtorDescription}
+                        onMouseLeave={hideRealtorDescription}>
+                        <NameProfile className="card choose-profile-realtor">
+                            <button className="profileButton">Consultor</button>
+                        </NameProfile>
+                    </Link>
 
-                <Link className="link choose-profile-agency" href="/sign-up">
-                    <ProfileContainer className="card choose-profile-agency">
-                        <button className="profileButton">Agência</button>
-                    </ProfileContainer>
-                </Link>
+                    <Link className="link choose-profile-agency" href="/sign-up" // const sign up agency
+                        onMouseEnter={showAgencyDescription}
+                        onMouseLeave={hideAgencyDescription}>
+                        <NameProfile className="card choose-profile-agency">
+                            <button className="profileButton">Agência</button>
+                        </NameProfile>
+                    </Link>
 
-            </ChooseProfileContainer>
+                </ViewProfile>
 
-            <ProfileDescriptionContainer className="card description-client-profile">
-                <h4>Está em busca de um consultor para conquistar seu próximo imóvel?</h4>
-            </ProfileDescriptionContainer>
+                <DescriptionProfile className="card description-client-profile">
+                    <h3>Está em busca de um consultor para conquistar seu próximo imóvel?</h3>
+                </DescriptionProfile>
 
-            <ProfileDescriptionContainer className="card description-client-realtor">
-                <h3>Crie seu perfil e atraia clientes</h3>
-            </ProfileDescriptionContainer>
+                <DescriptionProfile className="card description-realtor-profile">
+                    <h3>Divulgue suas vendas, atraia clientes e se associe a sua empresa</h3>
+                </DescriptionProfile>
 
-            <ProfileDescriptionContainer className="card description-client-agency">
-                <h3>Cadastre sua empresa para associar seus consultores e divulgar seus imóveis a venda</h3>
-            </ProfileDescriptionContainer>
+                <DescriptionProfile className="card description-agency-profile">
+                    <h3>Cadastre sua empresa para associar seus consultores e divulgar seus imóveis a venda</h3>
+                </DescriptionProfile>
+
+            </ChooseProfile>
+
 
         </Layout>
     );
