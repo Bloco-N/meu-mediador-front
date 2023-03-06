83651cb3fafd105a5cacc94c3dfa55d282009c17
Sara-MF <sara.melo@aluno.uece.br>
2023-03-05 21:18:57 -0300

    feat: types for profiles forms

---
+++ src/types/MyProfileAgency.ts
@@ -0,0 +1,16 @@
+export type MyProfileAgency = {
+    name: string,
+    contact: string,
+    address: string,
+    description: string,
+    areas: string,
+    languages: string,
+    team: string,
+    housesToSale: string,
+    site: string,
+    facebook: string,
+    instagram: string,
+    twitter: string,
+    specializations: string,
+    services: string
+}
