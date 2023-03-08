5146bcdd551696951103d27e4d402c9b85eb7af2
iueu <ilwelisaac@hotmail.com>
2023-03-08 19:55:59 -0300

    feat: search page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,13 @@
 import { useForm } from "react-hook-form";
 import styled from "styled-components";
+import SearchContext from "context/SearchContext";
+import { SearchContextType } from "@/types/SearchContextType";
+import { useRouter } from "next/router";
+import { useContext } from "react";
+import SearchResultContext from "context/SearchResultContext";
+import { SearchResultContextType } from "@/types/SearchResultContextType";
+import { SearchForm } from "@/types/SearchForm";
+import { UserCard } from "@/types/UserCard";
 
 const SearchRealtor = styled.div`
   
@@ -24,16 +32,56 @@ const SearchRealtor = styled.div`
 
 export default function Home() {
 
-  const { register } = useForm()
+  const { register, handleSubmit } = useForm<SearchForm>()
+
+  const router = useRouter()
+
+  const { setSearch } = useContext(SearchContext) as SearchContextType
+  const { setSearchResult } = useContext(SearchResultContext) as SearchResultContextType
+
+  
+  
+  const onSubmit = async (data:SearchForm) =>{
+    const fetchData = async () => {
+      let url = process.env.NEXT_PUBLIC_API_URL + '/realtor?'
+      if(data.search){
+        url += 'search=' + data.search
+        setSearch(data.search)
+      } else{
+        setSearch('')
+      }
+      const response = await fetch(url, {
+        method:'GET'
+      })
+      const json = await response.json()
+      const searchList = [] as UserCard []
+      for(const item of json.list){
+        searchList.push({
+          id: item.id,
+          firstName: item.firstName,
+          lastName: item.lastName
+        })
+      }
+      setSearchResult({
+        list: searchList,
+        currentPage: json.currentPage,
+        totalOfPages: json.totalOfPages
+      })
+      router.push('/search-result')
+    }
+    await fetchData()
+  }
 
   return (
 
     <SearchRealtor>
 
-      <form className="card" action="">
+      <form className="card" onSubmit={handleSubmit(onSubmit)}>
         <div className="search-row">
-          <input className="input-realtor" placeholder="Nome do Consultor" />
-          <input className="input-city-cep" placeholder="Cidade ou CEP" />
+          <input className="input-realtor" placeholder="Nome do Consultor"
+          {...register('search')} />
+          <input className="input-city-cep" placeholder="Cidade ou CEP" 
+          {...register('zipCode')}/>
           <button className="searchButton">Buscar</button>
         </div>
         <h4>Encontre um consultor pro seu próximo imóvel aqui</h4>
