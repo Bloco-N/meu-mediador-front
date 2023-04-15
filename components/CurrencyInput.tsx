f8bb6bfcf054d89ea010e9c47c3249992f72bf26
iueu <ilwelisaac@hotmail.com>
2023-04-15 15:16:25 -0300

    feat: new input masks

---
+++ components/CurrencyInput.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import MaskedInput from 'react-text-mask'
+import createNumberMask from 'text-mask-addons/dist/createNumberMask'
+
+const defaultMaskOptions = {
+  prefix: '',
+  suffix: ' €',
+  includeThousandsSeparator: true,
+  thousandsSeparatorSymbol: '.',
+  allowDecimal: true,
+  decimalSymbol: ',',
+  decimalLimit: 2, // how many digits allowed after the decimal
+  integerLimit: 8, // limit length of integer numbers
+  allowNegative: false,
+  allowLeadingZeroes: false,
+}
+ 
+const CurrencyInput = ({ ...inputProps }) => {
+  const currencyMask = createNumberMask(defaultMaskOptions)
+
+  return <MaskedInput mask={currencyMask} {...inputProps} />
+}
+ 
+export default CurrencyInput
